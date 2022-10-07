d1bec3ebbe3267347a3919a35efcf29b3f588d21
Melanie Beer <melanie.beer@students.fhnw.ch>
2022-10-07 14:54:26 +0200

    Uebung03

---
+++ Uebung03/colortext.js
@@ -0,0 +1,13 @@
+function faerben() {
+    console.log("Text wird gefärbt")
+
+    var random = Math.floor(Math.random()*16777215).toString(16);
+
+    var farbe = "#" + random;
+
+    var d = document.getElementById("output");
+    d.style.color = farbe;
+    d.style.fontSize = '150px';
+    d.innerHTML = document.querySelector("input").value;
+   
+}
